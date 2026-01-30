91f048dc7345e4a2c3264fa3289d6bc00e2414e2
Mishanya22222 <mpolishchuk930@gmail.com>
2026-01-30 12:42:59 +0000

    creating baseball repo

---
+++ 28th_Jan/create.py
@@ -0,0 +1,19 @@
+# cd .. is for going back to the parent directory
+# import sqlite3 and pandas
+
+import sqlite3
+import pandas as pd
+
+# Create a new SQLite database (or connect to an existing one)
+conn = sqlite3.connect('baseball.db')
+cursor = conn.cursor()
+
+# Read data from a CSV file and store it in the df variable
+df = pd.read_csv('Batting(1).csv')
+# Write the DataFrame to a SQL table named 'batting'
+# batting is the name of the table
+df.to_sql('batting', conn, if_exists='replace', index=False)
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
---
+++ 28th_Jan/fetch.py
@@ -0,0 +1,18 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect("baseball.db")
+cursor = conn.cursor()
+
+query = """ 
+    SELECT playerID, yearID, teamID, HR
+    FROM batting
+    WHERE HR >20;
+"""
+
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+
+records_df = pd.DataFrame(records, columns=['playerID', 'yearID', 'teamID', 'HR'])
+print(records_df)
